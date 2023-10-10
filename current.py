49659205b30204ec820f851af843414959d70407
trouw <116220815+trouw@users.noreply.github.com>
2023-10-09 23:17:55 -0400

    Update current.py

--- current.py
+++ current.py
@@ -56,7 +56,7 @@ def obtain_access_token():
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "redirect_uris": [
+            "redirect_uri": [
                 "https://keyword-intent-clusters.streamlit.app"
             ]
         }
