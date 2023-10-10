4d60ef71f28c9e9033fda1175fe5959b52c634cf
trouw <116220815+trouw@users.noreply.github.com>
2023-10-09 22:17:30 -0400

    Update current.py

--- current.py
+++ current.py
@@ -22,6 +22,11 @@ GSC_API_NAME = 'webmasters'
 
 keyword_data_df = pd.DataFrame(columns=['Keyword', 'Clicks', 'Impressions', 'CTR', 'Position'])
 
+# Global variables for OAuth2
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+PROJECT_ID = ''
+
 # Function to convert the dataframe to a downloadable format
 def to_csv_download_link(df, filename="data.csv"):
     csv = df.to_csv(index=False)
@@ -29,12 +34,33 @@ def to_csv_download_link(df, filename="data.csv"):
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV File</a>'
     return href
 
+# Function to get OAuth2 details from the user
+def get_oauth2_details():
+    global CLIENT_ID, CLIENT_SECRET, PROJECT_ID
+    
+    CLIENT_ID = st.text_input("Enter your Client ID:")
+    CLIENT_SECRET = st.text_input("Enter your Client Secret:", type="password")
+    PROJECT_ID = st.text_input("Enter your Project ID:")
+
 # Function to obtain an access token
 def obtain_access_token():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        'your-credentials.json',  # Path to your client secrets JSON file
-        SCOPES
-    )
+    # Use global CLIENT_ID and CLIENT_SECRET
+    global CLIENT_ID, CLIENT_SECRET, PROJECT_ID
+    
+    # Prepare the client_config
+    client_config = {
+        "installed": {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "project_id": PROJECT_ID,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "redirect_uris": [
+        "https://keyword-intent-clusters.streamlit.app/"]
+        }
+    }
+
+    flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
 
     credentials_obj = flow.run_local_server(port=0)
 
@@ -337,18 +363,30 @@ def main():
     if 'show_dataframe' not in st.session_state:
         st.session_state.show_dataframe = False
 
-    # Button to obtain the access token
-    with st.expander("Obtain Access Token"):
-        if st.button("Obtain Access Token"):
-            access_token = obtain_access_token()
-            if access_token is not None:
-                st.session_state.access_token = access_token
+    if 'oauth2_set' not in st.session_state:
+        st.session_state.oauth2_set = False
 
-        st.info("Click the button above to obtain an access token.")
+        # Input the OAuth2 details
+        with st.expander("Enter OAuth2 Details"):
+            get_oauth2_details()
 
-        if 'access_token' not in st.session_state or st.session_state.access_token is None:
-            st.warning("Access token not available. Please obtain an access token.")
-            return
+            if st.button("Set OAuth2 Details"):
+                st.session_state.oauth2_set = True
+                st.success("OAuth2 details set successfully!")
+
+        # Only allow the user to obtain an access token if OAuth2 details are set
+        if st.session_state.oauth2_set:
+            with st.expander("Obtain Access Token"):
+                if st.button("Obtain Access Token"):
+                    access_token = obtain_access_token()
+                    if access_token is not None:
+                        st.session_state.access_token = access_token
+
+                st.info("Click the button above to obtain an access token.")
+
+                if 'access_token' not in st.session_state or st.session_state.access_token is None:
+                    st.warning("Access token not available. Please obtain an access token.")
+                    return
 
     # Initially, only show Keyword Data Query section
     with st.expander("Keyword Data Query", expanded=True):
