e3f7d8df16d2832c0ee93d8cc1db119a957fa99f
trouw <116220815+trouw@users.noreply.github.com>
2023-10-14 21:43:27 -0400

    Update current.py

--- current.py
+++ current.py
@@ -325,9 +325,8 @@ def create_clusters_clicks_impressions(similarity_df):
         st.write(keywords)
         st.write('////////////')
         keyword_filter = similarity_df['Keyword_B'].isin(keywords) & similarity_df['Keyword_B'].isin(keywords)
-        st.write(len(keyword_filter))
         keyword_data = similarity_df[keyword_filter]
-        # st.write(keyword_data)
+        st.write(len(keyword_data))
         total_volume = keyword_data['clicks'].sum()
         total_imps = keyword_data['impressions'].sum()
         avg_intent = keyword_data['Keyword Intent'].mean()
