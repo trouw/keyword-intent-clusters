198aac6d37c70f7d9b7e5c5704431f75fe433ce4
trouw <116220815+trouw@users.noreply.github.com>
2023-10-14 20:48:46 -0400

    Update current.py

--- current.py
+++ current.py
@@ -278,8 +278,8 @@ def create_clusters_search_volume(similarity_df):
     
     cluster_data = []
     for cluster, keywords in clusters.items():
-        st.write(cluster)
         keywords.append(cluster)
+        st.write(keyword)
         keyword_data = similarity_df[similarity_df['Keyword'].isin(keywords) | similarity_df['Keyword_B'].isin(keywords)]
         total_volume = keyword_data['Search Volume'].sum()
         avg_intent = keyword_data['Keyword Intent'].mean()
