afc306bbd63c0bdb8ead513ee08229fb3c33fc7f
trouw <116220815+trouw@users.noreply.github.com>
2023-10-21 16:59:35 -0400

    Update current.py

--- current.py
+++ current.py
@@ -361,19 +361,20 @@ def create_bubble_chart(agg_data):
 
     # Now create the bubble chart
     sizes = agg_data[str(st.session_state.size_metric)]
-    cluster_names = agg_data['Cluster Name']  # Extract cluster names
     fig, ax = plt.subplots()
 
     # Set the y-axis limits
-    ax.set_ylim(-2, len(cluster_names) + 1)
-    
-    # Scatter plot with numeric y-axis values and labels at the center of bubbles
-    y_values = np.arange(len(cluster_names))
+    ax.set_ylim([-2, 12])
+
+    # Generate numeric y-axis values
+    y_values = list(range(len(agg_data)))
+
+    # Scatter plot with y-axis as numeric values
     ax.scatter([0] * len(agg_data), y_values, s=sizes, alpha=0.5)
 
-    # Annotate each bubble with its cluster name at the center
-    for i, name in enumerate(cluster_names):
-        ax.annotate(name, (0, i), ha='center', va='center', fontsize=8)
+    # Add cluster names to the center of bubbles
+    for i, label in enumerate(agg_data['Cluster Name']):
+        ax.text(0, y_values[i], label, ha='center', va='center')
 
     # Enable auto-sizing
     ax.autoscale(enable=True, axis='y', tight=True)
@@ -382,7 +383,7 @@ def create_bubble_chart(agg_data):
     image_url = 'https://static.semrush.com/blog/uploads/media/9a/51/9a51504510308d6515f6f858c396e8be/original.png'
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
-    ax.imshow(image, extent=[-10, 10, -2, len(cluster_names) + 1], alpha=0.5)  # Adjust extent and alpha as needed
+    ax.imshow(image, extent=[-10, 10, -2, 12], alpha=0.5)  # Adjust extent and alpha as needed
 
     # Show the chart
     st.pyplot(fig)
