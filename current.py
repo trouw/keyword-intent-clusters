1d65b825416a27e8ddf308fbf902d71beec448af
trouw <116220815+trouw@users.noreply.github.com>
2023-10-21 12:55:52 -0400

    Update current.py

--- current.py
+++ current.py
@@ -166,7 +166,7 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
 
     # Create a DataFrame from the combined data for all keywords
     df = pd.DataFrame(all_data, columns=["Keyword", "URL", "Position", "Title", "Description", "Keyword Intent"])
-    
+    print(df)
     return df
 
 def extract_info_from_serp(data):
