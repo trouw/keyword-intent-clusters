4eca06a3a493a142303f9a1cd83ee848193e3063
trouw <116220815+trouw@users.noreply.github.com>
2023-10-21 17:19:52 -0400

    Update current.py

--- current.py
+++ current.py
@@ -375,7 +375,7 @@ def create_bubble_chart(agg_data, x_limit, y_limit, font_size):
     image_url = 'https://static.semrush.com/blog/uploads/media/9a/51/9a51504510308d6515f6f858c396e8be/original.png'
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
-    ax.imshow(image, extent=[-x_limit, x_limit, -y_limit, y_limit], alpha=0.5)
+    ax.imshow(image, extent=[-x_limit, x_limit, 0, 10], alpha=0.5)
 
     # Show the chart
     st.pyplot(fig)
