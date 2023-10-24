a2a3c1b924ee48bd179d830981480a8170d7c030
trouw <116220815+trouw@users.noreply.github.com>
2023-10-24 02:15:44 -0400

    Update current.py

--- current.py
+++ current.py
@@ -103,7 +103,7 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
                 response_ready = client.get("/v3/serp/google/organic/tasks_ready")
                 time.sleep(10)
                 st.write('refrsh)')
-                st.write(len(response_ready['tasks'][0][results]))
+                
 
         st.write('complete')
         
