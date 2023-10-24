a7f5bce90c5651ab3f72599dc3db650b4caff96c
trouw <116220815+trouw@users.noreply.github.com>
2023-10-24 00:55:43 -0400

    Update current.py

--- current.py
+++ current.py
@@ -92,6 +92,7 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
             results = []
             while len(response_ready['tasks']) != 0:
                 response_ready = client.get("/v3/serp/google/organic/tasks_ready")
+                st.write(len(response_ready['tasks']))
                 for task in response_ready['tasks']:
                     if (task['result'] and (len(task['result']) > 0)):
                         for resultTaskInfo in task['result']:
