2164e36982bb4853732028ad77ac0ad5806f4f7d
trouw <116220815+trouw@users.noreply.github.com>
2023-10-24 09:59:56 -0400

    Update current.py

--- current.py
+++ current.py
@@ -107,9 +107,9 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
                             if resultTaskInfo['endpoint_advanced']:
                                 result = client.get(resultTaskInfo['endpoint_advanced'])
                                 results.append(result)
-                                print("Progress:", progress)
 
                                 progress = len(results) / len(keywords)
+                                st.write("Progress:", progress)
                                 progress_bar.progress(progress)
                 
                 # Check tasks readiness in the next iteration
