2b7ef0d15743ff85fd834c7844e195ffb3594331
trouw <116220815+trouw@users.noreply.github.com>
2023-10-25 12:09:24 -0400

    Update current.py

--- current.py
+++ current.py
@@ -93,30 +93,37 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
         # Send a batch of keywords as tasks
         response = send_batch_task(keyword_batch)
 
-    if response["status_code"] == 20000:
-        for x in response['tasks']:
-            id = x['id']
-            result_id.append(id)
+    results = []
+    result_id = []
 
-        response_ready = client.get("/v3/serp/google/organic/tasks_ready")
-        if response_ready["status_code"] == 20000:
-            progress_bar = st.progress(0)
-            while len(results) != len(keywords):
-                for task in response_ready['tasks']:
-                    if (task['result'] and (len(task['result']) > 0)):
-                        for resultTaskInfo in task['result']:
-                            if resultTaskInfo['endpoint_advanced']:
-                                # Check if the result's ID is in the list of desired IDs
-                                if resultTaskInfo['id'] in result_id:
-                                    result = client.get(resultTaskInfo['endpoint_advanced'])
-                                    results.append(result)
+    for keyword_batch in keyword_batches:
+        # Send a batch of keywords as tasks
+        response = send_batch_task(keyword_batch)
 
-                                    progress = len(results) / len(keywords)
-                                    # Ensure progress does not exceed 1.0
-                                    if progress > 1.0:
-                                        progress = 1.0
+        if response["status_code"] == 20000:
+            for x in response['tasks']:
+                id = x['id']
+                result_id.append(id)
 
-                                    progress_bar.progress(progress)
+    response_ready = client.get("/v3/serp/google/organic/tasks_ready")
+    if response_ready["status_code"] == 20000:
+        progress_bar = st.progress(0)
+        while len(results) != len(keywords):
+            for task in response_ready['tasks']:
+                if (task['result'] and (len(task['result']) > 0)):
+                    for resultTaskInfo in task['result']:
+                        if resultTaskInfo['endpoint_advanced']:
+                            # Check if the result's ID is in the list of desired IDs
+                            if resultTaskInfo['id'] in result_id:
+                                result = client.get(resultTaskInfo['endpoint_advanced'])
+                                results.append(result)
+
+                                progress = len(results) / len(keywords)
+                                # Ensure progress does not exceed 1.0
+                                if progress > 1.0:
+                                    progress = 1.0
+
+                                progress_bar.progress(progress)
 
                 # Check tasks readiness in the next iteration
                 response_ready = client.get("/v3/serp/google/organic/tasks_ready")
