33d5700229c0f3b1d4f2db20e302e8b8e1d0c62a
trouw <116220815+trouw@users.noreply.github.com>
2023-10-25 20:50:52 -0400

    Update current.py

--- current.py
+++ current.py
@@ -93,6 +93,7 @@ def query_dataforseo_serp(username, password, keywords, search_engine="google", 
     # Send tasks in batches
     for keyword_batch in keyword_batches:
         response = send_batch_task(keyword_batch)
+        st.write(response)
         if response["status_code"] == 20000:
             for task in response['tasks']:
                 result_ids.append(task['id'])
